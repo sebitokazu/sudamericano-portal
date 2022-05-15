1e6baaae8debb3cf953d9d827b6a29d579267321
Sebastian Itokazu <sebitokazu@gmail.com>
2022-05-15 02:09:41 -0300

    Footer edition

--- components/Footer.js
+++ components/Footer.js
@@ -1,196 +1,77 @@
-import Link from "next/link";
+import Image from "next/image";
 
 export default function Footer() {
-    return (
-      <footer className="pb-4 text-gray-200">
-        <div className="max-w-5xl xl:max-w-5xl mx-auto divide-y divide-gray-900 px-4 sm:px-6 md:px-8">
-          <ul className="text-sm font-medium sm:pb-20 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-10">
-            <li className="space-y-5 row-span-2">
-              <h2 className="text-2xl tracking-wide text-white">Company</h2>
-              <ul className="space-y-4 text-md">
-                <li>
-                  <Link
-                    className="hover:text-white transition-colors duration-200 text-xl"
-                    href="/"
-                  >
-                    Jobs
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="hover:text-white transition-colors duration-200 text-xl"
-                    href="/"
-                  >
-                    Merch
-                  </Link>
-                </li>{" "}
-                <li>
-                  <Link
-                    className="hover:text-white transition-colors duration-200 text-xl"
-                    href="/"
-                  >
-                    Brand
-                  </Link>
-                </li>{" "}
-                <li>
-                  <Link
-                    className="hover:text-white transition-colors duration-200 text-xl"
-                    href="/"
-                  >
-                    Meetups
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li className="space-y-5 row-span-2">
-              <h2 className="text-2xl tracking-wide text-white">Newsroom</h2>
-              <ul className="space-y-4">
-                <li>
-                  <Link
-                    className="hover:text-white transition-colors duration-200 text-xl"
-                    href="/"
-                  >
-                    News
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="hover:text-white transition-colors duration-200 text-xl"
-                    href="/"
-                  >
-                    Press
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="hover:text-white transition-colors duration-200 text-xl"
-                    href="/"
-                  >
-                    Blog
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li className="space-y-5 row-span-2">
-              <h2 className="text-2xl tracking-wide text-white">Products</h2>
-              <ul className="space-y-4">
-                <li>
-                  <Link
-                    className="hover:text-white transition-colors duration-200 text-xl"
-                    href="/"
-                  >
-                    Hosting
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="hover:text-white transition-colors duration-200 text-xl"
-                    href="/"
-                  >
-                    Domains
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="hover:text-white transition-colors duration-200 text-xl"
-                    href="/"
-                  >
-                    Security
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="hover:text-white transition-colors duration-200 text-xl"
-                    href="/"
-                  >
-                    SSL
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li className="space-y-5">
-              <h2 className="text-2xl tracking-wide text-white">Connect</h2>
-              <ul className="space-y-4">
-                <li>
-                  <Link
-                    className="hover:text-white transition-colors duration-200 text-xl"
-                    href="/"
-                  >
-                    Twitter
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="hover:text-white transition-colors duration-200 text-xl"
-                    href="/"
-                  >
-                    Facebook
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="hover:text-white transition-colors duration-200 font-semibold text-xl"
-                    href="/"
-                  >
-                    Instagram
-                  </Link>
-                </li>
-              </ul>
-            </li>
-          </ul>
-          <div className="flex flex-col-reverse justify-between pt-5 pb-4 border-t lg:flex-row bg-top border-black">
-            <ul className="flex flex-col space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
-              <li>
-                <Link
-                  href="/"
-                  className="text-md text-gray-200 hover:text-white transition-colors duration-300 hover:text-deep-purple-accent-400 font-semibold"
-                >
-                  Terms of Service
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/"
-                  className="text-md text-gray-200 hover:text-white transition-colors duration-300 hover:text-deep-purple-accent-400 font-semibold"
-                >
-                  Privacy Policy
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/"
-                  className="text-md text-gray-200 hover:text-white transition-colors duration-300 hover:text-deep-purple-accent-400 font-semibold"
-                >
-                  Ad Choices
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/"
-                  className="text-md text-gray-200 hover:text-white transition-colors duration-300 hover:text-deep-purple-accent-400 font-semibold"
-                >
-                  Cookie Policy
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/"
-                  className="text-md text-gray-200 hover:text-white transition-colors duration-300 hover:text-deep-purple-accent-400 font-semibold"
-                >
-                  Partners
-                </Link>
-              </li>
-            </ul>
-            <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
-              <Link
-                href="/"
-                className="text-md text-gray-200 hover:text-white transition-colors duration-300 hover:text-deep-purple-accent-400 font-semibold tracking-tight"
-              >
-                © 2021 Company Inc.
-              </Link>
-            </ul>
+  return (
+    <footer className="pb-4 text-gray-200">
+      <section id="#sponsors">
+        <div className="container px-5 mx-auto lg:px-24 ">
+          <div className="flex flex-col w-full mb-4 text-left lg:text-center">
+            <h1 className="mb-8 text-2xl font-bold text-white ">
+              Nos acompañan
+            </h1>
+          </div>
+          <div className="flex flex-row flex-wrap justify-center mx-auto gap-4  lg:grid-cols-4">
+            <div className="flex items-center justify-center">
+              <img
+                src="/images/Cloudflare-Logo.svg"
+                alt="Argentina Secretaria Deportes Logo"
+                className="block object-contain h-16 greyC"
+              ></img>
+            </div>
+            <div className="col-span-3 flex items-center justify-center">
+              <img
+                src="/images/PayPal-Logo.png"
+                alt="La Matanza Secretaria Deportes Logo"
+                className="block object-contain h-16 greyC"
+              ></img>
+            </div>
+            <div className="flex items-center justify-center">
+              <img
+                src="/images/Cloudflare-Logo.svg"
+                alt="CAS Logo"
+                className="block object-contain h-16 greyC"
+              ></img>
+            </div>
+            <div className="flex items-center justify-center">
+              <img
+                src="/images/Cloudflare-Logo.svg"
+                alt="WBSC Logo"
+                className="block object-contain h-16 greyC"
+              ></img>
+            </div>
+            <div className="flex items-center justify-center">
+              <img
+                src="/images/PayPal-Logo.png"
+                alt="ASBA Logo"
+                className="block object-contain h-16 greyC"
+              ></img>
+            </div>
+            <div className="flex items-center justify-center">
+              <img
+                src="/images/Google-Logo.webp"
+                alt="Tornado Logo"
+                className="block object-contain h-16 greyC"
+              ></img>
+            </div>
+            <div className="flex items-center justify-center">
+              <img
+                src="/images/Shopify-Logo.svg"
+                alt="Estadio de la Victoria Logo"
+                className="block object-contain h-16 greyC"
+              ></img>
+            </div>
           </div>
         </div>
-      </footer>
-    );
-  }
+      </section>
+      <section id="contacto"></section>
+      <div className="flex flex-row justify-center">
+        <Image
+          src="/logo.png"
+          alt="Sudamericano Logo"
+          width={250}
+          height={250}
+        />
+      </div>
+    </footer>
+  );
+}
