8c4596f6baf0d1ad16410fdd4cf649dfbc165e1b
Sebastian Itokazu <sebitokazu@gmail.com>
2022-05-14 21:38:45 -0300

    Base template + navbar styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,21 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors:{
-        'suda-blue': '#013189'
-      }
+      colors: {
+        "suda-blue": "#043f9d",
+        gray: {
+          100: "#FBFBFB",
+          200: "#c2c7ca",
+          300: "#b8bcbf",
+          400: "#999999",
+          500: "#7F7F7F",
+          600: "#666666",
+          700: "#4C4C4C",
+          800: "#121212",
+          900: "#191919",
+        },
+      },
     },
   },
   plugins: [],
-}
+};
