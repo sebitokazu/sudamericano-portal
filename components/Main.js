3874fb01a564c214ce33c0dcc7e6627994d04fb5
Sebastian Itokazu <sebitokazu@gmail.com>
2022-05-16 18:45:18 -0300

    Contacto facebook

--- components/Main.js
+++ components/Main.js
@@ -140,43 +140,6 @@ export default function Main() {
               />
             </div>
             <div className="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center self-center">
-              {/* <div className="flex flex-col mb-10 lg:items-start items-center">
-                <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-gray-800 text-lime-200 mb-5">
-                  <Image
-                    src="/softball-ball.png"
-                    alt="Pelota de softbol"
-                    className="white-icon"
-                    width="30"
-                    height="30"
-                  />
-                </div>
-                <div className="flex-grow">
-                  <h2 className="text-slate-100 text-xl font-bold mb-3">
-                    Templo del Softbol
-                  </h2>
-                  <p className="leading-relaxed text-base text-slate-300">
-                  </p>
-                  <a
-                    href="https://www.instagram.com/estadiodelavictoria.softbol/"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    className="mt-3 text-lime-300 hover:text-cyan-100 inline-flex items-center"
-                  >
-                    Conoce más
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="w-4 h-4 ml-2"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M5 12h14M12 5l7 7-7 7"></path>
-                    </svg>
-                  </a>
-                </div>
-              </div> */}
               <div className="flex flex-col mb-10 lg:items-start items-center">
                 <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-gray-800 text-indigo-400 mb-5">
                   <Image
@@ -215,45 +178,6 @@ export default function Main() {
                   </a>
                 </div>
               </div>
-              {/* <div className="flex flex-col mb-10 lg:items-start items-center">
-              <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-gray-800 text-indigo-400 mb-5">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  className="w-6 h-6"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                  <circle cx="12" cy="7" r="4"></circle>
-                </svg>
-              </div>
-              <div className="flex-grow">
-                <h2 className="text-white text-lg title-font font-medium mb-3">
-                  Neptune
-                </h2>
-                <p className="leading-relaxed text-base">
-                  Blue bottle crucifix vinyl post-ironic four dollar toast vegan
-                  taxidermy. Gastropub indxgo juice poutine.
-                </p>
-                <a className="mt-3 text-indigo-400 inline-flex items-center">
-                  Learn More
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    className="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M5 12h14M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
-              </div>
-            </div> */}
             </div>
           </div>
         </div>
@@ -271,11 +195,11 @@ export default function Main() {
         </div>
       </section>
       <section id="redes" className="pt-40 -mt-40">
-        <div className="max-w-6xl pt-12 mb-1 pb-16 mx-auto">
+        <div className="max-w-6xl pt-12 mb-1 pb-6 mx-auto">
           <h4 className="text-xl font-semibold tracking-tighter text-center text-gray-100 lg:text-3xl md:text-2xl">
             Seguinos en nuestras redes
           </h4>
-          <div className="flex flex-row flex-wrap mx-auto justify-center py-12">
+          <div className="flex flex-row flex-wrap mx-auto gap-8 justify-center py-12">
             <a
               href="https://www.instagram.com/softbol.sudamericano.argentina/"
               rel="noopener noreferrer"
@@ -290,6 +214,22 @@ export default function Main() {
                 viewBox="0 0 16 16"
               >
                 <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
+              </svg>
+            </a>
+            <a
+              href="https://www.facebook.com/softbol.sudamericano.argentina/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <svg
+                title="Facebook"
+                xmlns="http://www.w3.org/2000/svg"
+                width="48"
+                height="48"
+                className="white-icon"
+                viewBox="0 2 25 25"
+              >
+<path d="M19,3H5C3.895,3,3,3.895,3,5v14c0,1.105,0.895,2,2,2h7.621v-6.961h-2.343v-2.725h2.343V9.309 c0-2.324,1.421-3.591,3.495-3.591c0.699-0.002,1.397,0.034,2.092,0.105v2.43h-1.428c-1.13,0-1.35,0.534-1.35,1.322v1.735h2.7 l-0.351,2.725h-2.365V21H19c1.105,0,2-0.895,2-2V5C21,3.895,20.105,3,19,3z"></path>
               </svg>
             </a>
           </div>
